9918f4ff7b3fe721f4ed986677b9ed4706dfbfba
Leonardo Lopes <leonardo.lopes@fapi-pinhais.edu.br>
2023-09-11 21:33:09 -0300

    add header and other test routes

--- taskmaster-app/config/app.php
+++ taskmaster-app/config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://localhost/taskmaster/taskmaster-app/public/'),
 
     'asset_url' => env('ASSET_URL'),
 
--- taskmaster-app/routes/web.php
+++ taskmaster-app/routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('header') . view('welcome');
+});
+
+Route::get('/task', function () {
+    return view('header') . view('task');
+});
+
+Route::get('/ranking', function () {
+    return view('header') . view('ranking');
 });
