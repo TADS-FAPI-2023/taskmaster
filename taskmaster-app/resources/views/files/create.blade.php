47de72190ee147e695e6af53a83501fe0e71dd22
dbsiqueira <diegodbds13@gmail.com>
2023-10-06 19:45:44 -0300

    a

--- taskmaster-app/resources/views/files/create.blade.php
+++ taskmaster-app/resources/views/files/create.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
-@section('title','Task')
-@section('content_header','Task')
+@section('title', 'Tarefa')
+@section('content_header', 'Tarefa')
 @section('content')
-    <a class="btn btn-info btn-xl" href="{{ route('files.index') }}">Go Back</a><br><br>
+    <a class="btn btn-info btn-xl" href="{{ route('files.index') }}">Voltar</a><br><br>
     <form action="{{ route('files.store') }}" method="post" enctype="multipart/form-data">
         @csrf
         <div class="form-group col-xl-6 col-lg-6 col-md-8 col-sm-8 col-12">
-            <label for="filename">Code<small> (Only one file at a time) *</small></label>
-            <input type="file" class="form-control @error('filename') is-invalid @enderror" name="filename">
+            <label for="filename">Arquivo RAR<small> (Apenas um arquivo por vez) *</small></label>
+            <input type="file" class="form-control @error('filename') is-invalid @enderror" name="filename" accept=".rar">
             @if ($errors->has('filename'))
                 <span class="invalid-feedback">
                     <strong>{{ $errors->first('filename') }}</strong>
@@ -16,9 +16,9 @@
         </div>
         <br>
         <div class="form-group col-xl-6 col-lg-6 col-md-8 col-sm-8 col-12">
-            <label for="description">Description *</label>
+            <label for="description">Descrição *</label>
             <textarea type="description" rows="4" class="form-control @error('description') is-invalid @enderror"
-                      name="description" placeholder="Type description about this file">{{ old('description') }}</textarea>
+                      name="description" placeholder="Digite uma descrição sobre este arquivo">{{ old('description') }}</textarea>
             @if ($errors->has('description'))
                 <span class="invalid-feedback">
                     <strong>{{ $errors->first('description') }}</strong>
@@ -27,7 +27,7 @@
         </div>
         <div class="form-group">
             <br>
-            <button class="btn btn-primary btn-xl"><i class="fa-solid fa-share"></i> Send</button>
+            <button class="btn btn-primary btn-xl"><i class="fa-solid fa-share"></i> Enviar</button>
         </div>
     </form>
 @endsection
--- taskmaster-app/resources/views/files/index.blade.php
+++ taskmaster-app/resources/views/files/index.blade.php
@@ -1,47 +1,42 @@
 @extends('layouts.app')
-@section('title', 'Task')
-@section('content_header', 'Task')
+@section('title', 'Tarefa')
+@section('content_header', 'Tarefa')
 @section('content')
-    <a class="btn btn-primary btn-xl" href="{{ route('files.create') }}"><i class="fa-solid fa-plus"></i> Add project</a><br><br>
+    <a class="btn btn-primary btn-xl" href="{{ route('files.create') }}"><i class="fa-solid fa-plus"></i> Adicionar projeto</a><br><br>
     @if ($files->count() > 0)
         <div class="row">
             @foreach ($files as $item)
                 <div class="col">
                     <div class="card" style="width: 18rem;">
-                        @if (in_array($item->extension, ['.jpg', '.jpeg', '.png', '.gif']))
-                            <img src="{{ $item->file_url }}" class="card-img-top">
-                        @else
                             <img src="{{ asset('img/arquivox.png') }}" class="card-img-top">
-                        @endif
                         <div class="card-body">
                             <h5 class="card-title">{{ $item->extension }}</h5>
                             <p class="card-text">{{ $item->description }}</p>
                             <a href="{{ route('files.edit', $item->id) }}" class="btn btn-warning"> <i
-                                    class="fa-solid fa-pen-clip"></i> Edit File</a>
+                                    class="fa-solid fa-pen-clip"></i> Editar Arquivo</a>
                             <button type="button" class="btn btn-danger" data-bs-toggle="modal"
                                 data-bs-target="#modal-file-{{ $item->id }}">
-                                <i class="fa-solid fa-ban"></i> Delete File
+                                <i class="fa-solid fa-ban"></i> Excluir Arquivo
                             </button>
                             <div class="modal fade" id="modal-file-{{ $item->id }}" tabindex="-1"
                                 aria-labelledby="label-file-{{ $item->id }}" aria-hidden="true">
                                 <div class="modal-dialog">
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <h5 class="modal-title" id="label-file-{{ $item->id }}">Delete File</h5>
+                                            <h5 class="modal-title" id="label-file-{{ $item->id }}">Excluir Arquivo</h5>
                                             <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                aria-label="Close"></button>
+                                                aria-label="Fechar"></button>
                                         </div>
                                         <div class="modal-body">
-                                            <img src="{{ $item->file_url }}" style="width: 30%;"><br><br>
-                                            <h5>Are you sure you want to delete this file?</h5>
+                                            <h5>Tem certeza de que deseja excluir este arquivo?</h5>
                                         </div>
                                         <div class="modal-footer">
                                             <button type="button" class="btn btn-secondary"
-                                                data-bs-dismiss="modal">Cancel</button>
+                                                data-bs-dismiss="modal">Cancelar</button>
                                             <form action="{{ route('files.destroy', $item->id) }}" method="post">
                                                 @csrf
                                                 @method('delete')
-                                                <button type="submit" class="btn btn-danger">Delete</button>
+                                                <button type="submit" class="btn btn-danger">Excluir</button>
                                             </form>
                                         </div>
                                     </div>
@@ -54,7 +49,7 @@
         </div>
     @else
     <div class="alert alert-info" role="alert">
-        You don't have any files, click <b>"Add project"</b>.
+        Você não possui arquivos, clique em <b>"Adicionar projeto"</b>.
     </div>
 @endif
 
