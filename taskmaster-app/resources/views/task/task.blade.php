7263dfa9d035dc3391146f07229d74aa89232119
dbsiqueira <diegodbds13@gmail.com>
2023-10-24 20:26:32 -0300

    add migrations active to project n status to task

--- taskmaster-app/resources/views/task/task.blade.php
+++ taskmaster-app/resources/views/task/task.blade.php
@@ -4,10 +4,17 @@
         <input name="project_id" type="hidden" value="<?= $project->id ?>">
         <input type="submit" class="btn btn-primary" value="Cadastrar Tarefa">
     </form>
+    <select class="form-control" id="statusFilter">
+        <option value="ativo" selected>Ativo</option>
+        <option value="concluido">Concluido</option>
+        <option value="inativo">Inativo</option>
+    </select>
 </div>
 
 <div class="container mt-4">
     @foreach ($tasks as $key => $task)
+
+
         @if ($key % 3 == 0)
             <div class="row">
         @endif
@@ -30,6 +37,7 @@
                 <p>Descrição: <br> {{ $task->description }}</p>
                 <p>Prazo de Tempo: {{ $task->time_limit }}</p>
                 <p>Dificuldade: {{ $task->difficulty }}</p>
+                     <p id='status'>Status: {{ $task->status }}</p>
                 {{-- <a class="btn btn-primary" href="{{ url('/tarefa/' . $task->id) }}">Ver</a> --}}
                 <a class="btn btn-primary" href="{{ url('/taskform/' . $task->id) }}">Editar</a>
             </div>
@@ -39,3 +47,29 @@
     @endif
     @endforeach
 </div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const statusFilter = document.getElementById('statusFilter');
+        const taskContainers = document.querySelectorAll('.container-fluid');
+
+        statusFilter.addEventListener('change', function () {
+            const selectedStatus = statusFilter.value;
+
+            taskContainers.forEach(function (taskContainer) {
+                const statusElement = taskContainer.querySelector('p[id^="status"]');
+                console.log(taskContainer.querySelector('p[id^="status"]'));
+
+                if (statusElement) {
+                    const taskStatus = statusElement.textContent.split(' ')[1];
+
+                    if (selectedStatus === 'todos' || selectedStatus === taskStatus) {
+                        taskContainer.style.display = 'block';
+                    } else {
+                        taskContainer.style.display = 'none';
+                    }
+                }
+            });
+        });
+    });
+</script>
