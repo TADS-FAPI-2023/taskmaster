0df31858e81c2bb80c5c4f57b5a38e151a76f05d
Diegodbds <diegodbds13@gmail.com>
2023-11-26 21:27:29 -0300

    Edit style forms

--- taskmaster-app/resources/views/task/formulario.blade.php
+++ taskmaster-app/resources/views/task/formulario.blade.php
@@ -1,5 +1,5 @@
-<div class="container mt-4 p-4" style="border-radius: 0.5rem; background: #1E1D27;">
-    <form class="" action="{{ isset($project) ? url('/formulario/' . $project->id) : url('/formulario') }}" method="post">
+<div class="container mt-4 p-4" style="border-radius: 0.5rem; background: #1E1D27;  font-family: Inter;">
+    <form action="{{ isset($project) ? url('/formulario/' . $project->id) : url('/formulario') }}" method="post">
         @csrf
 
         @if(isset($project))
@@ -10,15 +10,33 @@
             {{ method_field('PUT') }}
         @endif
 
-        <label for="name">Nome:</label>
-        <input class="form-control" type="text" id="name" name="name" value="{{ isset($project) ? $project->name : '' }}" required><br>
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <label for="name" class="text-light">Nome:</label>
+                <input class="form-control" type="text" id="name" name="name" value="{{ isset($project) ? $project->name : '' }}" required>
+            </div>
 
-        <label for="description">Descrição:</label>
-        <textarea class="form-control" id="description" name="description" rows="3" required>{{ isset($project) ? $project->description : '' }}</textarea><br>
+            <div class="col-12 col-md-6">
+                <label for="status" class="text-light">Status:</label>
+                <select class="form-control text-dark" id="status" name="status" required>
+                    <option value="Finalizar"  class="text-dark" {{ isset($project) && $project->status == 'Finalizar' ? 'selected' : '' }}>Finalizar</option>
+                    <option value="Pausar" class="text-dark" {{ isset($project) && $project->status == 'Pausar' ? 'selected' : '' }}>Pausar</option>
+                    <option value="Lançar" class="text-dark" {{ isset($project) && $project->status == 'Lançar' ? 'selected' : '' }}>Lançar</option>
+                </select>
+            </div>
+        </div>
 
-        <label for="status">Status:</label>
-        <input class="form-control" type="text" id="status" name="status" value="{{ isset($project) ? $project->status : '' }}" required><br>
+        <div class="row mt-3">
+            <div class="col-12 col-md-6">
+                <label for="description" class="text-light">Descrição:</label>
+                <textarea class="form-control" id="description" name="description" rows="3" required>{{ isset($project) ? $project->description : '' }}</textarea>
+            </div>
+        </div>
 
-        <input class="btn btn-primary" type="submit" value="{{ isset($project) ? 'Editar' : 'Cadastrar' }}">
+        <div class="row mt-3">
+            <div class="col-12">
+                <input class="btn btn-primary" type="submit" value="{{ isset($project) ? 'Editar' : 'Cadastrar' }}">
+            </div>
+        </div>
     </form>
 </div>
--- taskmaster-app/resources/views/task/taskform.blade.php
+++ taskmaster-app/resources/views/task/taskform.blade.php
@@ -1,6 +1,5 @@
-<div class="container mt-4 p-4" style="border-radius: 0.5rem; background: #1E1D27;">
-
-    <form action="{{ isset($task_id) ? route('updateTask', $task_id) : url('/sendTaskForm') }}" method="post" style=" ">
+<div class="container mt-4 p-4" style="border-radius: 0.5rem; background: #1E1D27; >
+    <form action="{{ isset($task_id) ? route('updateTask', $task_id) : url('/sendTaskForm') }}" method="post">
         @csrf
 
         <!-- Utilize o método PUT para atualização -->
@@ -8,24 +7,48 @@
             @method('PUT')
         @endif
 
-        <input name="project_id" hidden value="{{ $project_id }}">
+        <input name="project_id" type="hidden" value="{{ $project_id }}">
         <input name="task_id" type="hidden" value="{{ $task_id ?? '' }}">
 
-        <label for="name">Nome:</label>
-        <input class="form-control" type="text" id="name" name="name" value="{{ $task_name ?? '' }}" required><br>
+        <div class="row mb-3">
+            <div class="col-12 col-md-6">
+                <label for="name" class="form-label text-light">Nome:</label>
+                <input class="form-control" type="text" id="name" name="name" value="{{ $task_name ?? '' }}" required>
+            </div>
 
-        <label for="type">Tipo:</label>
-        <input class="form-control" type="text" id="type" name="type" value="{{ $task_type ?? '' }}" required><br>
+            <div class="col-12 col-md-6">
+                <label for="type" class="form-label text-light">Tipo:</label>
+                <input class="form-control" type="text" id="type" name="type" value="{{ $task_type ?? '' }}" required>
+            </div>
+        </div>
 
-        <label for="description">Descrição:</label>
-        <textarea class="form-control" id="description" name="description" rows="3" required>{{ $task_description ?? '' }}</textarea><br>
+        <div class="row mb-3">
+            <div class="col-12">
+                <label for="description" class="form-label text-light">Descrição:</label>
+                <textarea class="form-control" id="description" name="description" rows="3" required>{{ $task_description ?? '' }}</textarea>
+            </div>
+        </div>
 
-        <label for="time_limit">Prazo de Tempo "Em dias":</label>
-        <input class="form-control" type="date" id="time_limit" name="time_limit" value="{{ $task_time_limit ?? '' }}" required><br>
+        <div class="row mb-3">
+            <div class="col-12 col-md-6">
+                <label for="time_limit" class="form-label text-light">Prazo de Tempo "Em dias":</label>
+                <input class="form-control" type="date" id="time_limit" name="time_limit" value="{{ $task_time_limit ?? '' }}" min="{{ date('Y-m-d') }}" required>
+            </div>
 
-        <label for="difficulty">Dificuldade:</label>
-        <input class="form-control" type="text" id="difficulty" name="difficulty" value="{{ $task_difficulty ?? '' }}" required><br>
+            <div class="col-12 col-md-6">
+                <label for="difficulty" class="form-label text-light">Dificuldade:</label>
+                <select class="form-control text-dark" id="difficulty" name="difficulty" required>
+                    <option value="Facil" {{ isset($task_difficulty) && $task_difficulty == 'Facil' ? 'selected' : '' }}>Fácil</option>
+                    <option value="Medio" {{ isset($task_difficulty) && $task_difficulty == 'Medio' ? 'selected' : '' }}>Médio</option>
+                    <option value="Dificil" {{ isset($task_difficulty) && $task_difficulty == 'Dificil' ? 'selected' : '' }}>Difícil</option>
+                </select>
+            </div>
+        </div>
 
-        <input class="btn btn-primary" type="submit" value="{{ isset($task_id) ? 'Atualizar' : 'Cadastrar' }}">
+        <div class="row">
+            <div class="col-12">
+                <button class="btn btn-primary" type="submit">{{ isset($task_id) ? 'Atualizar' : 'Cadastrar' }}</button>
+            </div>
+        </div>
     </form>
 </div>
